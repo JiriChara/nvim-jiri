1dc6d91d0d1ab16b011cb7e03df9fcdf042ba8c6
Jiri Chara <jiri.chara@pm.me>
2025-11-01 21:22:59 +0100

    Add lazy.nvim

---
+++ nvim/lua/config/plugins.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup()
+    end
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  }
+}
