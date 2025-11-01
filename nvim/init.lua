e883382bb1693c804e5d8b0633612fa34e2146b9
Jiri Chara <jiri.chara@pm.me>
2025-11-01 19:17:05 +0100

    Add good options for Neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,11 @@
-print("Hello World")
+-- Safely enable Neovim's Lua module loader (speeds up startup; won't error on older versions)
+pcall(vim.loader.enable)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("config.options")
+require("config.globals")
+-- require("config.keymaps")
+-- require("config.autocmds")
+-- require("config.plugins")
